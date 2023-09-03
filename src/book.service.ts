94fd685cc382871e03a1c87ebd553497eee86395
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-09-03 16:59:36 +0200

    feat: load books from server

--- src/book.service.ts
+++ src/book.service.ts
@@ -1,15 +1,44 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Book, compareWithTitle } from './Book';
 import { readFile } from 'fs/promises';
+import { HttpService } from '@nestjs/axios';
+import { first, firstValueFrom, map, tap } from 'rxjs';
+import { BookDTO } from './BookDTO';
 
 @Injectable()
 export class BookService implements OnModuleInit {
   private readonly storage = new Map<string, Book>();
 
+  constructor(private readonly httpService: HttpService) {}
+
   async onModuleInit() {
+    await Promise.all([this.loadBooksFromFile(), this.loadBooksFromServer()]);
+    console.log(`Storage contains ${this.storage.size} books`);
+  }
+
+  private async loadBooksFromFile() {
     const data = await readFile('src/dataset.json');
     const books: Array<Book> = JSON.parse(data.toString());
     books.forEach((book) => this.addBook(book));
+  }
+
+  private async loadBooksFromServer(): Promise<void> {
+    return firstValueFrom(
+      this.httpService.get('https://api.npoint.io/fbb2a6039fc21e320b30').pipe(
+        map((response) => response.data),
+        tap((books: BookDTO[]) => {
+          books.forEach((book) =>
+            this.addBook({
+              title: book.title,
+              author: book.authors,
+              date: book.publication_date,
+              isbn: book.isbn,
+            }),
+          );
+        }),
+        map(() => undefined),
+      ),
+    );
   }
 
   addBook(book: Book) {
